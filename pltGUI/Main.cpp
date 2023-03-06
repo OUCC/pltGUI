a85530ba7c939da504f6ebae7588442827cd39c4
MrMocchy <patronummocchy@tiara.ocn.n.jp>
2023-03-06 17:10:27 +0900

    OpenSiv3Dプロジェクトの作成とgitignoreの作成
    gitignoreは https://github.com/Siv3D/OpenSiv3D/blob/main/.gitignore に加えて、exampleの全内容

---
+++ pltGUI/Main.cpp
@@ -0,0 +1,86 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.6
+
+void Main()
+{
+	// 背景の色を設定 | Set background color
+	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
+
+	// 通常のフォントを作成 | Create a new font
+	const Font font{ 60 };
+
+	// 絵文字用フォントを作成 | Create a new emoji font
+	const Font emojiFont{ 60, Typeface::ColorEmoji };
+
+	// `font` が絵文字用フォントも使えるようにする | Set emojiFont as a fallback
+	font.addFallback(emojiFont);
+
+	// 画像ファイルからテクスチャを作成 | Create a texture from an image file
+	const Texture texture{ U"example/windmill.png" };
+
+	// 絵文字からテクスチャを作成 | Create a texture from an emoji
+	const Texture emoji{ U"🐈"_emoji };
+
+	// 絵文字を描画する座標 | Coordinates of the emoji
+	Vec2 emojiPos{ 300, 150 };
+
+	// テキストを画面にデバッグ出力 | Print a text
+	Print << U"Push [A] key";
+
+	while (System::Update())
+	{
+		// テクスチャを描く | Draw a texture
+		texture.draw(200, 200);
+
+		// テキストを画面の中心に描く | Put a text in the middle of the screen
+		font(U"Hello, Siv3D!🚀").drawAt(Scene::Center(), Palette::Black);
+
+		// サイズをアニメーションさせて絵文字を描く | Draw a texture with animated size
+		emoji.resized(100 + Periodic::Sine0_1(1s) * 20).drawAt(emojiPos);
+
+		// マウスカーソルに追随する半透明な円を描く | Draw a red transparent circle that follows the mouse cursor
+		Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1, 0, 0, 0.5 });
+
+		// もし [A] キーが押されたら | When [A] key is down
+		if (KeyA.down())
+		{
+			// 選択肢からランダムに選ばれたメッセージをデバッグ表示 | Print a randomly selected text
+			Print << Sample({ U"Hello!", U"こんにちは", U"你好", U"안녕하세요?" });
+		}
+
+		// もし [Button] が押されたら | When [Button] is pushed
+		if (SimpleGUI::Button(U"Button", Vec2{ 640, 40 }))
+		{
+			// 画面内のランダムな場所に座標を移動
+			// Move the coordinates to a random position in the screen
+			emojiPos = RandomVec2(Scene::Rect());
+		}
+	}
+}
+
+//
+// - Debug ビルド: プログラムの最適化を減らす代わりに、エラーやクラッシュ時に詳細な情報を得られます。
+//
+// - Release ビルド: 最大限の最適化でビルドします。
+//
+// - [デバッグ] メニュー → [デバッグの開始] でプログラムを実行すると、[出力] ウィンドウに詳細なログが表示され、エラーの原因を探せます。
+//
+// - Visual Studio を更新した直後は、プログラムのリビルド（[ビルド]メニュー → [ソリューションのリビルド]）が必要です。
+//
+// チュートリアル
+// https://siv3d.github.io/ja-jp/tutorial/tutorial/
+//
+// Tutorial
+// https://siv3d.github.io/tutorial/tutorial/
+//
+// Siv3D コミュニティへの参加（Discord などで気軽に質問や交流, 最新情報の入手ができます）
+// https://siv3d.github.io/ja-jp/community/community/
+//
+// Siv3D User Community
+// https://siv3d.github.io/community/community/
+//
+// 新機能の提案やバグの報告 | Feedback
+// https://siv3d.github.io/ja-jp/develop/report/
+//
+// Sponsoring Siv3D
+// https://github.com/sponsors/Reputeless
+//
